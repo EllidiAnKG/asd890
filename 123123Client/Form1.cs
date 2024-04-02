9364ee5361ecf4d540580c92a5b8d1d03fc1a3f3
321 <112854201+EllidiAnKG@users.noreply.github.com>
2024-04-02 23:31:08 +0500

    Add files via upload

---
+++ 123123Client/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _123123Client
+{
+    public partial class Form1 : Form
+    {
+
+        private TcpClient client;
+        private NetworkStream stream;
+        private string localIp;
+        private string remoteIp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            localIp = GetLocalIPAddress();
+            
+        }
+
+        private string GetLocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            remoteIp = textBox4.Text;
+            client = new TcpClient(remoteIp, 12345);
+            stream = client.GetStream();
+
+            Thread receiveThread = new Thread(ReceiveMessages);
+            receiveThread.Start();
+        }
+        private void ReceiveMessages()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                        Invoke((MethodInvoker)delegate
+                        {
+                            textBox2.Text += $"{message}" + Environment.NewLine;
+                        });
+                    }
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при чтении данных: {ex.Message}");
+                    break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = textBox1.Text;
+                if (client.Connected)
+                {
+                    byte[] mes = Encoding.UTF8.GetBytes($"{localIp}:{message}");
+                    stream.Write(mes, 0, mes.Length);
+                    textBox2.Text += $"{localIp}: {message}" + Environment.NewLine;
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка: Соединение разорвано");
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"Произошла ошибка при записи данных: {ex.Message}");
+            }
+        }
+
+       
+
+        
+    }
+}
---
+++ 12321Server/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static TcpListener server;
+    static List<TcpClient> clients = new List<TcpClient>();
+    static string historyFolderPath = "ChatHistory";
+
+    static async Task Main(string[] args)
+    {
+        server = new TcpListener(IPAddress.Any, 12345);
+        server.Start();
+
+        Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+        while (true)
+        {
+            TcpClient client = await server.AcceptTcpClientAsync();
+            clients.Add(client);
+            NetworkStream stream = client.GetStream();
+            Console.WriteLine("Новый клиент подключен.");
+
+            Task.Run(async () => await ReceiveAndBroadcastMessages(stream, client));
+        }
+    }
+
+    static async Task ReceiveAndBroadcastMessages(NetworkStream fromStream, TcpClient fromClient)
+    {
+        while (true)
+        {
+            try
+            {
+                byte[] data = new byte[1024];
+                int bytesRead = await fromStream.ReadAsync(data, 0, data.Length);
+                string message = Encoding.UTF8.GetString(data, 0, bytesRead);
+
+                Console.WriteLine("Новое сообщение от клиента: " + message);
+
+                foreach (var client in clients)
+                {
+                    if (client != fromClient && client.Connected)
+                    {
+                        NetworkStream toStream = client.GetStream();
+                        byte[] responseData = Encoding.UTF8.GetBytes(message);
+                        await toStream.WriteAsync(responseData, 0, responseData.Length);
+                    }
+                }
+
+                string historyFilePath = Path.Combine(historyFolderPath, $"{((IPEndPoint)fromClient.Client.RemoteEndPoint).Address}_chat_history.txt");
+                SaveMessageToHistory(historyFilePath, ((IPEndPoint)fromClient.Client.RemoteEndPoint).Address.ToString(), message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при чтении/отправке сообщения: " + ex.Message);
+                break;
+            }
+        }
+    }
+
+    static void SaveMessageToHistory(string filePath, string ipAddress, string message)
+    {
+        if (!Directory.Exists(historyFolderPath))
+        {
+            Directory.CreateDirectory(historyFolderPath);
+        }
+
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine($"{DateTime.Now} - IP: {ipAddress}, Message: {message}");
+        }
+    }
+}
+
